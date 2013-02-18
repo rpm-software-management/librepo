fb7f37b731b8b044930afc9c11f2a05564b0120c
Tomas Mlcoch <tmlcoch@redhat.com>
2013-02-18 12:11:44 +0100

    Update librepo.pc.cmake

--- librepo/librepo.pc.cmake
+++ librepo/librepo.pc.cmake
@@ -5,7 +5,7 @@ includedir=@CMAKE_INSTALL_PREFIX@/include
 Name: librepo
 Description: Repodata downloading library.
 Version: @VERSION@
-Requires.private: libcurl
+Requires.private: libcurl openssl
 Libs: -L${libdir} -lrepo
-Libs.private: -lcurl -lexpat
+Libs.private: -lexpat -gpgme -gpg-error
 Cflags: -I${includedir} -D_FILE_OFFSET_BITS=64
