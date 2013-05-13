bd35cf0f8ecb8f3aa6289348449b305504bc4f53
Tomas Mlcoch <tmlcoch@redhat.com>
2013-05-13 09:47:57 +0200

    py: Change reported name from _librepo.Exception to librepo.LibrepoException.

--- librepo/python/exception-py.c
+++ librepo/python/exception-py.c
@@ -25,7 +25,7 @@ PyObject *LrErr_Exception = NULL;
 int
 init_exceptions()
 {
-    LrErr_Exception = PyErr_NewException("_librepo.Exception", NULL, NULL);
+    LrErr_Exception = PyErr_NewException("librepo.LibrepoException", NULL, NULL);
     if (!LrErr_Exception)
         return 0;
     Py_INCREF(LrErr_Exception);
