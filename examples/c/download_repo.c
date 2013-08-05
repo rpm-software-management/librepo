0c21a505bc1a6e30580a75a4874141ab68f9c2cd
Tomas Mlcoch <tmlcoch@redhat.com>
2013-08-05 12:30:20 +0200

    Update c example

--- examples/c/download_repo.c
+++ examples/c/download_repo.c
@@ -1,3 +1,4 @@
+#include <glib.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <librepo/librepo.h>
@@ -5,12 +6,12 @@
 int
 main(void)
 {
-    int ret = EXIT_SUCCESS, rc;
-    lr_Handle h;
-    lr_Result r;
+    int rc = EXIT_SUCCESS;
+    gboolean ret;
+    LrHandle *h;
+    LrResult *r;
     char *download_list[] = LR_YUM_HAWKEY;
-
-    lr_global_init();
+    GError *tmp_err = NULL;
 
     h = lr_handle_init();
     r = lr_result_init();
@@ -19,15 +20,15 @@ main(void)
     lr_handle_setopt(h, LRO_REPOTYPE, LR_YUMREPO);
     lr_handle_setopt(h, LRO_YUMDLIST, download_list);
 
-    rc = lr_handle_perform(h, r);
-    if (rc != LRE_OK) {
-        fprintf(stderr, "Error encountered: %d (%s)\n", rc, lr_strerror(rc));
-        ret = EXIT_FAILURE;
+    ret = lr_handle_perform(h, r, &tmp_err);
+    if (!ret) {
+        fprintf(stderr, "Error encountered: %d: %s\n",
+                tmp_err->code, tmp_err->message);
+        rc = EXIT_FAILURE;
     }
 
     lr_result_free(r);
     lr_handle_free(h);
-    lr_global_cleanup();
 
-    return ret;
+    return rc;
 }
