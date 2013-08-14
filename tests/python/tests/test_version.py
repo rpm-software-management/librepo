4e8128138b75dc33e77eac9cd530231aee5b0357
Tomas Mlcoch <tmlcoch@redhat.com>
2013-08-14 13:51:38 +0200

    tests: Fix python tests

--- tests/python/tests/test_version.py
+++ tests/python/tests/test_version.py
@@ -3,6 +3,5 @@ import librepo
 
 class TestCaseVersion(unittest.TestCase):
     def test_version(self):
-        self.assertIsInstance(librepo.VERSION, str)
         self.assertGreaterEqual(len(librepo.VERSION), 5)
         self.assertEqual(librepo.VERSION.count('.'), 2)
