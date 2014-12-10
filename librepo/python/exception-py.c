7aaefbdf35d7d410b30575224d920f272c0b2148
Tomas Mlcoch <tmlcoch@redhat.com>
2014-12-10 09:06:13 +0100

    python: Put Python.h first

--- librepo/python/exception-py.c
+++ librepo/python/exception-py.c
@@ -18,9 +18,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <Python.h>
 #include <glib.h>
 #include <glib/gprintf.h>
-#include <Python.h>
 #include "exception-py.h"
 
 PyObject *LrErr_Exception = NULL;
--- librepo/python/result-py.c
+++ librepo/python/result-py.c
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <glib.h>
 #include <Python.h>
+#include <glib.h>
 #undef NDEBUG
 #include <assert.h>
 
