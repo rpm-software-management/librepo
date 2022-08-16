rpm-software-management/librepo

commit d2508e206514bdbf841ee72f4971336766c16fe1

More covscan fixes

librepo/metadata_downloader.c
@@ -191,7 +191,7 @@ handle_failure(LrMetadataTarget *target,
                GSList **paths,
                GError *err)
 {
-    lr_metadatatarget_append_error(target, err->message, NULL);
+    lr_metadatatarget_append_error(target, err->message);
     fillInvalidationValues(fd_list, paths);
     g_error_free(err);
 }
@@ -220,13 +220,13 @@ create_repomd_xml_download_targets(GSList *targets,
         handle = target->handle;
 
         if (!handle->urls && !handle->mirrorlisturl && !handle->metalinkurl) {
-            lr_metadatatarget_append_error(target, "No LRO_URLS, LRO_MIRRORLISTURL nor LRO_METALINKURL specified", NULL);
+            lr_metadatatarget_append_error(target, "No LRO_URLS, LRO_MIRRORLISTURL nor LRO_METALINKURL specified");
             fillInvalidationValues(fd_list, paths);
             continue;
         }
 
         if (handle->repotype != LR_YUMREPO) {
-            lr_metadatatarget_append_error(target, "Bad LRO_REPOTYPE specified", NULL);
+            lr_metadatatarget_append_error(target, "Bad LRO_REPOTYPE specified");
             fillInvalidationValues(fd_list, paths);
             continue;
         }
@@ -241,14 +241,14 @@ create_repomd_xml_download_targets(GSList *targets,
         if (!lr_handle_prepare_internal_mirrorlist(handle,
                                                    handle->fastestmirror,
                                                    &err)) {
-            lr_metadatatarget_append_error(target, "Cannot prepare internal mirrorlist: %s", err->message, NULL);
+            lr_metadatatarget_append_error(target, "Cannot prepare internal mirrorlist: %s", err->message);
             fillInvalidationValues(fd_list, paths);
             g_error_free(err);
             continue;
         }
 
         if (mkdir(handle->destdir, S_IRWXU) == -1 && errno != EEXIST) {
-            lr_metadatatarget_append_error(target, "Cannot create tmpdir: %s %s", handle->destdir, g_strerror(errno), NULL);
+            lr_metadatatarget_append_error(target, "Cannot create tmpdir: %s %s", handle->destdir, g_strerror(errno));
             fillInvalidationValues(fd_list, paths);
             g_error_free(err);
             continue;
@@ -334,12 +334,12 @@ process_repomd_xml(GSList *targets,
         handle->gnupghomedir = g_strdup(target->gnupghomedir);
 
         if (target->download_target->rcode != LRE_OK) {
-            lr_metadatatarget_append_error(target, (char *) lr_strerror(target->download_target->rcode), NULL);
+            lr_metadatatarget_append_error(target, (char *) lr_strerror(target->download_target->rcode));
             goto fail;
         }
 
         if (!lr_check_repomd_xml_asc_availability(handle, target->repo, fd_value, path->data, &error)) {
-            lr_metadatatarget_append_error(target, error->message, NULL);
+            lr_metadatatarget_append_error(target, error->message);
             g_error_free(error);
             goto fail;
         }
@@ -348,7 +348,7 @@ process_repomd_xml(GSList *targets,
         ret = lr_yum_repomd_parse_file(target->repomd, fd_value, lr_xml_parser_warning_logger,
                                        "Repomd xml parser", &error);
         if (!ret) {
-            lr_metadatatarget_append_error(target, "Parsing unsuccessful: %s", error->message, NULL);
+            lr_metadatatarget_append_error(target, "Parsing unsuccessful: %s", error->message);
             g_error_free(error);
             goto fail;
         }
@@ -376,7 +376,7 @@ lr_metadata_download_cleanup(GSList *download_targets)
         LrDownloadTarget *download_target = elem->data;
         LrMetadataTarget *target = download_target->userdata;
         if (download_target->err)
-            lr_metadatatarget_append_error(target, download_target->err, NULL);
+            lr_metadatatarget_append_error(target, download_target->err);
 
         if (target->err != NULL) {
             ret = FALSE;

librepo/metalink.c
@@ -504,7 +504,7 @@ lr_metalink_parse_file(LrMetalink *metalink,
 
     // Parsing
 
-    ret = lr_xml_parser_generic(parser, pd, fd, &tmp_err);
+    ret = lr_xml_parser_generic(&parser, pd, fd, &tmp_err);
     if (tmp_err) {
         g_propagate_error(err, tmp_err);
         goto err;

librepo/repomd.c
@@ -570,7 +570,7 @@ lr_yum_repomd_parse_file(LrYumRepoMd *repomd,
 
     // Parsing
 
-    ret = lr_xml_parser_generic(parser, pd, fd, &tmp_err);
+    ret = lr_xml_parser_generic(&parser, pd, fd, &tmp_err);
     if (tmp_err)
         g_propagate_error(err, tmp_err);
 

librepo/xmlparser.c
@@ -143,7 +143,7 @@ lr_xml_parser_strtoll(LrParserData *pd,
 }
 
 gboolean
-lr_xml_parser_generic(XmlParser parser,
+lr_xml_parser_generic(XmlParser *parser,
                       LrParserData *pd,
                       int fd,
                       GError **err)
@@ -151,7 +151,7 @@ lr_xml_parser_generic(XmlParser parser,
     /* Note: This function uses .err members of LrParserData! */
 
     gboolean ret = TRUE;
-    xmlParserCtxtPtr ctxt = xmlCreatePushParserCtxt(&parser, pd, NULL, 0, NULL);
+    xmlParserCtxtPtr ctxt = xmlCreatePushParserCtxt(parser, pd, NULL, 0, NULL);
     ctxt->linenumbers = 1;
 
     assert(ctxt);

librepo/xmlparser_internal.h
@@ -159,7 +159,7 @@ lr_xml_parser_strtoll(LrParserData *pd,
 /** Generic parser.
  */
 gboolean
-lr_xml_parser_generic(XmlParser parser,
+lr_xml_parser_generic(XmlParser *parser,
                       LrParserData *pd,
                       int fd,
                       GError **err);

librepo/yum.c
@@ -335,7 +335,7 @@ lr_prepare_repodata_dir(LrHandle *handle,
             return FALSE;
         }
     }
-    lr_free(path_to_repodata);
+    g_free(path_to_repodata);
 
     return TRUE;
 }
@@ -356,7 +356,7 @@ lr_store_mirrorlist_files(LrHandle *handle,
             g_debug("%s: Cannot create: %s", __func__, ml_file_path);
             g_set_error(err, LR_YUM_ERROR, LRE_IO,
                         "Cannot create %s: %s", ml_file_path, g_strerror(errno));
-            lr_free(ml_file_path);
+            g_free(ml_file_path);
             return FALSE;
         }
         rc = lr_copy_content(handle->mirrorlist_fd, fd);
@@ -366,7 +366,7 @@ lr_store_mirrorlist_files(LrHandle *handle,
             g_set_error(err, LR_YUM_ERROR, LRE_IO,
                         "Cannot copy content of mirrorlist file %s: %s",
                         ml_file_path, g_strerror(errno));
-            lr_free(ml_file_path);
+            g_free(ml_file_path);
             return FALSE;
         }
         repo->mirrorlist = ml_file_path;
@@ -391,7 +391,7 @@ lr_copy_metalink_content(LrHandle *handle,
             g_debug("%s: Cannot create: %s", __func__, ml_file_path);
             g_set_error(err, LR_YUM_ERROR, LRE_IO,
                         "Cannot create %s: %s", ml_file_path, g_strerror(errno));
-            lr_free(ml_file_path);
+            g_free(ml_file_path);
             return FALSE;
         }
         rc = lr_copy_content(handle->metalink_fd, fd);
@@ -401,7 +401,7 @@ lr_copy_metalink_content(LrHandle *handle,
             g_set_error(err, LR_YUM_ERROR, LRE_IO,
                         "Cannot copy content of metalink file %s: %s",
                         ml_file_path, g_strerror(errno));
-            lr_free(ml_file_path);
+            g_free(ml_file_path);
             return FALSE;
         }
         repo->metalink = ml_file_path;
@@ -422,7 +422,7 @@ lr_prepare_repomd_xml_file(LrHandle *handle,
     if (fd == -1) {
         g_set_error(err, LR_YUM_ERROR, LRE_IO,
                     "Cannot open %s: %s", *path, g_strerror(errno));
-        lr_free(*path);
+        g_free(*path);
         return -1;
     }
 
@@ -458,13 +458,13 @@ lr_check_repomd_xml_asc_availability(LrHandle *handle,
             g_debug("%s: Cannot open: %s", __func__, signature);
             g_set_error(err, LR_YUM_ERROR, LRE_IO,
                         "Cannot open %s: %s", signature, g_strerror(errno));
-            lr_free(signature);
+            g_free(signature);
             return FALSE;
         }
 
         url = lr_pathconcat(handle->used_mirror, "repodata/repomd.xml.asc", NULL);
         ret = lr_download_url(handle, url, fd_sig, &tmp_err);
-        lr_free(url);
+        g_free(url);
         close(fd_sig);
         if (!ret) {
             // Error downloading signature
@@ -474,7 +474,7 @@ lr_check_repomd_xml_asc_availability(LrHandle *handle,
                         "repository does not support GPG verification: %s", tmp_err->message);
             g_clear_error(&tmp_err);
             unlink(signature);
-            lr_free(signature);
+            g_free(signature);
             return FALSE;
         } else {
             // Signature downloaded
@@ -483,7 +483,7 @@ lr_check_repomd_xml_asc_availability(LrHandle *handle,
                                          path,
                                          handle->gnupghomedir,
                                          &tmp_err);
-            lr_free(signature);
+            g_free(signature);
             if (!ret) {
                 g_debug("%s: GPG signature verification failed: %s",
                         __func__, tmp_err->message);
@@ -680,7 +680,7 @@ prepare_repo_download_std_target(LrHandle *handle,
                 __func__, *path, g_strerror(errno));
         g_set_error(err, LR_YUM_ERROR, LRE_IO,
                     "Cannot create/open %s: %s", *path, g_strerror(errno));
-        lr_free(*path);
+        g_free(*path);
         g_slist_free_full(*targets, (GDestroyNotify) lr_downloadtarget_free);
         return FALSE;
     }
@@ -713,7 +713,7 @@ prepare_repo_download_zck_target(LrHandle *handle,
                 __func__, *path, g_strerror(errno));
         g_set_error(err, LR_YUM_ERROR, LRE_IO,
                     "Cannot create/open %s: %s", *path, g_strerror(errno));
-        lr_free(*path);
+        g_free(*path);
         g_slist_free_full(*targets, (GDestroyNotify) lr_downloadtarget_free);
         return FALSE;
     }
@@ -778,7 +778,7 @@ prepare_repo_download_targets(LrHandle *handle,
         char *dest_dir = realpath(handle->destdir, NULL);
         path = lr_pathconcat(handle->destdir, record->location_href, NULL);
         char *requested_dir = realpath(dirname(path), NULL);
-        lr_free(path);
+        g_free(path);
         if (!g_str_has_prefix(requested_dir, dest_dir)) {
             g_debug("%s: Invalid path: %s", __func__, location_href);
             g_set_error(err, LR_YUM_ERROR, LRE_IO, "Invalid path: %s", location_href);
@@ -850,7 +850,7 @@ prepare_repo_download_targets(LrHandle *handle,
 
         /* Because path may already exists in repo (while update) */
         lr_yum_repo_update(repo, record->type, path);
-        lr_free(path);
+        g_free(path);
     }
 
     return TRUE;
@@ -1130,7 +1130,7 @@ lr_yum_use_local_load_base(LrHandle *handle,
             repo->mirrorlist = mrl_fn;
         } else {
             repo->mirrorlist = NULL;
-            lr_free(mrl_fn);
+            g_free(mrl_fn);
         }
     }
 
@@ -1142,7 +1142,7 @@ lr_yum_use_local_load_base(LrHandle *handle,
             repo->metalink = mtl_fn;
         } else {
             repo->metalink = NULL;
-            lr_free(mtl_fn);
+            g_free(mtl_fn);
         }
     }
 

tests/test_checksum.c
@@ -87,7 +87,7 @@ START_TEST(test_checksum_fd)
     test_checksum(file, LR_CHECKSUM_SHA512, CHKS_VAL_01_SHA512);
 
     ck_assert_msg(remove(file) == 0, "Cannot delete temporary test file");
-    lr_free(file);
+    g_free(file);
 }
 END_TEST
 
@@ -235,9 +235,9 @@ START_TEST(test_cached_checksum_value)
     ck_assert(attr_ret == -1);  // Cached checksum should not exists
 
     lr_free(calculated);
-    lr_free(filename);
-    lr_free(timestamp_key);
-    lr_free(checksum_key);
+    g_free(filename);
+    g_free(timestamp_key);
+    g_free(checksum_key);
     lr_free(mtime_str);
 }
 END_TEST
@@ -294,7 +294,7 @@ START_TEST(test_cached_checksum_clear)
     ck_assert(attr_ret != -1);
 cleanup:
     close(fd);
-    lr_free(filename);
+    g_free(filename);
     g_free(timestamp_key);
     g_free(checksum_key);
 }

tests/test_downloader.c
@@ -52,7 +52,7 @@ START_TEST(test_downloader_single_file)
     tmpfn1 = lr_pathconcat(test_globals.tmpdir, "single_file_XXXXXX", NULL);
 
     fd1 = mkstemp(tmpfn1);
-    lr_free(tmpfn1);
+    g_free(tmpfn1);
     ck_assert_int_ge(fd1, 0);
 
     t1 = lr_downloadtarget_new(handle, "index.html", NULL, fd1, NULL, NULL,
@@ -97,7 +97,7 @@ START_TEST(test_downloader_single_file_2)
     tmpfn1 = lr_pathconcat(test_globals.tmpdir, "single_file_2_XXXXXX", NULL);
 
     fd1 = mkstemp(tmpfn1);
-    lr_free(tmpfn1);
+    g_free(tmpfn1);
     ck_assert_int_ge(fd1, 0);
 
     t1 = lr_downloadtarget_new(NULL, "http://seznam.cz/index.html", NULL,
@@ -154,8 +154,8 @@ START_TEST(test_downloader_two_files)
 
     fd1 = mkstemp(tmpfn1);
     fd2 = mkstemp(tmpfn2);
-    lr_free(tmpfn1);
-    lr_free(tmpfn2);
+    g_free(tmpfn1);
+    g_free(tmpfn2);
     ck_assert_int_ge(fd1, 0);
     ck_assert_int_ge(fd2, 0);
 
@@ -223,9 +223,9 @@ START_TEST(test_downloader_three_files_with_error)
     fd1 = mkstemp(tmpfn1);
     fd2 = mkstemp(tmpfn2);
     fd3 = mkstemp(tmpfn3);
-    lr_free(tmpfn1);
-    lr_free(tmpfn2);
-    lr_free(tmpfn3);
+    g_free(tmpfn1);
+    g_free(tmpfn2);
+    g_free(tmpfn3);
     ck_assert_int_ge(fd1, 0);
     ck_assert_int_ge(fd2, 0);
     ck_assert_int_ge(fd3, 0);
@@ -329,7 +329,7 @@ START_TEST(test_downloader_checksum)
         tmpfn1 = lr_pathconcat(test_globals.tmpdir, "single_file_XXXXXX", NULL);
 
         fd1 = mkstemp(tmpfn1);
-        lr_free(tmpfn1);
+        g_free(tmpfn1);
         ck_assert_int_ge(fd1, 0);
 
         checksum = lr_downloadtargetchecksum_new(LR_CHECKSUM_SHA512,

commit b04543b0ca04191188e80ae9ae92acc6cd2d2ef5

Use g_strdup_vprintf() instead of manually calculating allocation space
Fixes an error introduced in d2508e206514bdbf841ee72f4971336766c16fe1 by
removing trailing NULLs, on which the size calculation code was relying.

Instead of this incosistent argument iteration, use g_strdup_vprintf(),
which allocates the new string correctly.

librepo/metadata_downloader.c
@@ -97,26 +97,11 @@ void
 lr_metadatatarget_append_error(LrMetadataTarget *target, char *format, ...)
 {
     va_list valist;
-    size_t length = strlen(format);
-    char *error_message = NULL;
-
     va_start(valist, format);
-    while (1) {
-        char *arg = va_arg(valist, char*);
-        if (arg == NULL)
-            break;
-
-        length += strlen(arg);
-    }
-    length += RESERVE;
+    gchar *error_message = g_strdup_vprintf(format, valist);
     va_end(valist);
 
-    va_start(valist, format);
-    error_message = malloc(length * sizeof(char));
-    vsnprintf(error_message, length, format, valist);
-    va_end(valist);
-
-    target->err = g_list_append(target->err, (gpointer) error_message);
+    target->err = g_list_append(target->err, error_message);
 }
 
 static gboolean