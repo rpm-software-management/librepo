ef7617598a4266e36ac9dbe10ad6e3d7188a4679
Jaroslav Rohel <jrohel@redhat.com>
2022-11-16 15:02:27 +0100

    PGP: Add functions for listing/exporting keys

--- librepo/gpg.c
+++ librepo/gpg.c
@@ -20,18 +20,31 @@
  */
 
 #include <assert.h>
-#include <glib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <string.h>
 #include <gpgme.h>
 #include <unistd.h>
 
 #include "rcodes.h"
 #include "util.h"
 #include "gpg.h"
+
+struct tLrGpgSubkey {
+    gboolean has_next;   // FALSE if this is the last subkey in the list
+    char *id;            // subkey id
+    char *fingerprint;   // fingerprint of the subkey in hex digit form
+    long int timestamp;  // creation timestamp, -1 if invalid, 0 if not available
+    gboolean can_sign;   // TRUE if subkey can be used for signing
+};
+
+struct tLrGpgKey {
+    gboolean has_next;     // FALSE if this is the last subkey in the list
+    char **uids;           // NULL terminated array of user IDs strings
+    LrGpgSubkey *subkeys;  // list of subkeys associated with the key. The first subkey is the primary key
+    char *raw_key;         // key in ACII-Armor format
+};
 
 /*
  * Creates the '/run/user/$UID' directory if it doesn't exist. If this
@@ -345,4 +358,229 @@ lr_gpg_import_key(const char *key_fn, const char *home_dir, GError **err)
     close(key_fd);
 
     return ret;
+}
+
+LrGpgKey *
+lr_gpg_list_keys(gboolean export_keys, const char *home_dir, GError **err)
+{
+    gpgme_error_t gpgerr;
+
+    gpgme_ctx_t context = lr_gpg_context_init(home_dir, err);
+    if (!context) {
+        return NULL;
+    }
+
+    GArray * keys = g_array_new(FALSE, FALSE, sizeof(LrGpgKey));
+
+    gpgerr = gpgme_op_keylist_start(context, NULL, 0);
+    while (gpg_err_code(gpgerr) == GPG_ERR_NO_ERROR) {
+        gpgme_key_t key;
+        gpgerr = gpgme_op_keylist_next(context, &key);
+        if (gpgerr) {
+            break;
+        }
+
+        GArray * subkeys = g_array_new(FALSE, FALSE, sizeof(LrGpgSubkey));
+        gpgme_subkey_t subkey = key->subkeys;
+        while (subkey) {
+            LrGpgSubkey lr_subkey;
+            lr_subkey.has_next = FALSE;
+            lr_subkey.id = g_strdup(subkey->keyid);
+            lr_subkey.fingerprint = g_strdup(subkey->fpr);
+            lr_subkey.timestamp = subkey->timestamp;
+            lr_subkey.can_sign = subkey->can_sign;
+            g_array_append_val(subkeys, lr_subkey);
+            subkey = subkey->next;
+        }
+         // All subkeys in the list except the last one are followed by another subkey
+        if (subkeys->len > 0) {
+            for (guint i = 0; i < subkeys->len - 1; ++i) {
+                g_array_index(subkeys, LrGpgSubkey, i).has_next = TRUE;
+            }
+        }
+
+        LrGpgKey lr_key;
+        lr_key.has_next = FALSE;
+
+        GPtrArray * uid_strings = g_ptr_array_new();
+        for (gpgme_user_id_t uids = key->uids; uids; uids = uids->next) {
+            if (!uids->uid) {
+                continue;
+            }
+            g_ptr_array_add(uid_strings, g_strdup(uids->uid));
+        }
+
+        gpgme_key_release(key);
+
+        g_ptr_array_add(uid_strings, NULL);  // add terminating NULL
+        lr_key.uids = (char **)g_ptr_array_free(uid_strings, FALSE);
+
+        lr_key.subkeys = (LrGpgSubkey *)(subkeys->len > 0 ? g_array_free(subkeys, FALSE) : g_array_free(subkeys, TRUE));
+        lr_key.raw_key = NULL;
+        g_array_append_val(keys, lr_key);
+    }
+    // All keys in the list except the last one are followed by another key
+    for (guint i = 0; i < keys->len - 1; ++i) {
+        g_array_index(keys, LrGpgKey, i).has_next = TRUE;
+    }
+
+    if (gpg_err_code(gpgerr) != GPG_ERR_EOF) {
+        g_debug("%s: gpgme_op_keylist_: %s",
+                 __func__, gpgme_strerror(gpgerr));
+        g_set_error(err, LR_GPG_ERROR, LRE_GPGERROR,
+                    "gpgme_op_keylist_ error: %s",
+                    gpgme_strerror(gpgerr));
+        lr_gpg_keys_free((LrGpgKey *)g_array_free(keys, FALSE));
+        gpgme_release(context);
+        return NULL;
+    }
+
+    gpgme_op_keylist_end(context);
+
+    LrGpgKey *lr_keys = (LrGpgKey *)(keys->len > 0 ? g_array_free(keys, FALSE) : g_array_free(keys, TRUE));
+
+    if (export_keys) {
+        for (LrGpgKey *lr_key = lr_keys; lr_key; ++lr_key) {
+            LrGpgSubkey *lr_subkey = lr_key->subkeys;
+            if (!lr_subkey) {
+                g_info("%s: Missing data to export key. Damaged key? Skipping the key", __func__);
+                if (!lr_key->has_next) {
+                    break;
+                }
+                continue;
+            }
+
+            gpgme_data_t key_data;
+            gpgerr = gpgme_data_new(&key_data);
+            if (gpgerr != GPG_ERR_NO_ERROR) {
+                g_debug("%s: gpgme_data_new: %s", __func__, gpgme_strerror(gpgerr));
+                g_set_error(err, LR_GPG_ERROR, LRE_GPGERROR,
+                            "gpgme_data_new() error: %s", gpgme_strerror(gpgerr));
+                lr_gpg_keys_free(lr_keys);
+                gpgme_release(context);
+                return NULL;
+            }
+
+            gpgerr = gpgme_op_export(context, lr_subkey->fingerprint, 0, key_data);
+            if (gpgerr != GPG_ERR_NO_ERROR) {
+                g_debug("%s: gpgme_op_export: %s", __func__, gpgme_strerror(gpgerr));
+                g_set_error(err, LR_GPG_ERROR, LRE_GPGERROR,
+                            "gpgme_op_export() error: %s", gpgme_strerror(gpgerr));
+                gpgme_data_release(key_data);
+                lr_gpg_keys_free(lr_keys);
+                gpgme_release(context);
+                return NULL;
+            }
+
+            off_t key_size = gpgme_data_seek(key_data, 0, SEEK_CUR);
+            gpgerr = gpgme_data_rewind(key_data);
+            if (gpgerr != GPG_ERR_NO_ERROR) {
+                g_debug("%s: gpgme_data_rewind: %s", __func__, gpgme_strerror(gpgerr));
+                g_set_error(err, LR_GPG_ERROR, LRE_GPGERROR,
+                            "gpgme_data_rewind() error: %s", gpgme_strerror(gpgerr));
+                gpgme_data_release(key_data);
+                lr_gpg_keys_free(lr_keys);
+                gpgme_release(context);
+                return NULL;
+            }
+
+            lr_key->raw_key = g_malloc0(key_size + 1);
+            ssize_t readed = gpgme_data_read(key_data, lr_key->raw_key, key_size);
+            if (readed == -1) {
+                g_debug("%s: gpgme_data_read: %s", __func__, gpgme_strerror(gpgerr));
+                g_set_error(err, LR_GPG_ERROR, LRE_GPGERROR,
+                            "gpgme_data_read() error: %s", gpgme_strerror(gpgerr));
+                gpgme_data_release(key_data);
+                lr_gpg_keys_free(lr_keys);
+                gpgme_release(context);
+                return NULL;
+            }
+            if (readed != key_size) {
+                g_warning("%s: Error exporting key \"%s\": gpgme_data_read: Key size is %ld but readed %ld. "
+                          "Skipping the key",
+                          __func__, lr_key->subkeys->fingerprint, (long)key_size, (long)readed);
+                g_free(lr_key->raw_key);
+                lr_key->raw_key = NULL;
+            }
+
+            gpgme_data_release(key_data);
+
+            if (!lr_key->has_next) {
+                break;
+            }
+        }
+    }
+
+    gpgme_release(context);
+    return lr_keys;
+}
+
+const LrGpgKey *
+lr_gpg_key_get_next(const LrGpgKey *key) {
+    return key->has_next ? ++key : NULL;
+}
+
+char * const *
+lr_gpg_key_get_userids(const LrGpgKey *key) {
+    return key->uids;
+}
+
+const char *
+lr_gpg_key_get_raw_key(const LrGpgKey *key) {
+    return key->raw_key;
+}
+
+const LrGpgSubkey *
+lr_gpg_key_get_subkeys(const LrGpgKey *key) {
+    return key->subkeys;
+}
+
+static void
+lr_gpg_subkeys_free(LrGpgSubkey *subkeys) {
+    for (LrGpgSubkey *item = subkeys; item; ++item) {
+        g_free(item->fingerprint);
+        g_free(item->id);
+        if (!item->has_next) {
+            break;
+        }
+    }
+    g_free(subkeys);
+}
+
+void
+lr_gpg_keys_free(LrGpgKey *keys) {
+    for (LrGpgKey *item = keys; item; ++item) {
+        g_free(item->raw_key);
+        lr_gpg_subkeys_free(item->subkeys);
+        g_strfreev(item->uids);
+        if (!item->has_next) {
+            break;
+        }
+    }
+    g_free(keys);
+}
+
+const LrGpgSubkey *
+lr_gpg_subkey_get_next(const LrGpgSubkey *subkey) {
+    return subkey->has_next ? ++subkey : NULL;
+}
+
+const char *
+lr_gpg_subkey_get_id(const LrGpgSubkey *subkey) {
+    return subkey->id;
+}
+
+const char *
+lr_gpg_subkey_get_fingerprint(const LrGpgSubkey *subkey) {
+    return subkey->fingerprint;
+}
+
+long int
+lr_gpg_subkey_get_timestamp(const LrGpgSubkey *subkey) {
+    return subkey->timestamp;
+}
+
+gboolean
+lr_gpg_subkey_get_can_sign(const LrGpgSubkey *subkey) {
+    return subkey->can_sign;
 }
--- librepo/gpg.h
+++ librepo/gpg.h
@@ -31,6 +31,14 @@ G_BEGIN_DECLS
  *  @{
  */
 
+/** A structure containing information about subkey.
+ */
+typedef struct tLrGpgSubkey LrGpgSubkey;
+
+/** A structure containing information about a key with subkeys.
+ */
+typedef struct tLrGpgKey LrGpgKey;
+
 /** Check detached signature of data.
  * @param signature_fd  File descriptor of signature file.
  * @param data_fd       File descriptor of data to verify.
@@ -102,6 +110,90 @@ gboolean
 lr_gpg_import_key(const char *key_fn,
                   const char *home_dir,
                   GError **err);
+
+
+/** List/export keys (and subkeys) from the keyring.
+ * @param export_keys   If TRUE, the list also contains the exported keys.
+ *                      Export is in ASCII-Armor format.
+ * @param home_dir      Configuration directory of OpenPGP engine
+ *                      (e.g. "/home/user/.gnupg/"), if NULL default
+ *                      config directory is used.
+ * @param err           GError **
+ * @return              returns list of keys (and subkeys), or NULL if keyring is empty or an error occured.
+ */
+LrGpgKey *
+lr_gpg_list_keys(gboolean export_keys,
+                 const char *home_dir,
+                 GError **err);
+
+/** Get the next key from the list obtained from lr_gpg_list_keys.
+ * @param key           Input key.
+ * @return              returns next kye.
+ */
+const LrGpgKey *
+lr_gpg_key_get_next(const LrGpgKey *key);
+
+/** Get NULL terminated array of user IDs strings.
+ * @param key           Input key.
+ * @return              returns NULL terminated array of user IDs strings.
+ */
+char * const *
+lr_gpg_key_get_userids(const LrGpgKey *key);
+
+/** Get key in ASCII-Armor format (only if `key` was obtained from lr_gpg_list_keys with `export = TRUE`).
+ * @param key           Input key.
+ * @return              returns key in ACII-Armor format.
+ */
+const char *
+lr_gpg_key_get_raw_key(const LrGpgKey *key);
+
+/** Get a list of subkeys associated with the key. The first subkey is the primary key.
+ * @param key           Input key.
+ * @return              returns list of subkeys associated with the key.
+ */
+const LrGpgSubkey *
+lr_gpg_key_get_subkeys(const LrGpgKey *key);
+
+/** Release the list of keys obtained from lr_gpg_list_keys.
+ * @param key           Input array of keys.
+ */
+void
+lr_gpg_keys_free(LrGpgKey *key_array);
+
+/** Get the next subkey from the list obtained from lr_gpg_key_get_subkeys.
+ * @param key           Input subkey.
+ * @return              returns next subkye.
+ */
+const LrGpgSubkey *
+lr_gpg_subkey_get_next(const LrGpgSubkey *subkey);
+
+/** Get subkey ID.
+ * @param key           Input subkey.
+ * @return              returns key ID.
+ */
+const char *
+lr_gpg_subkey_get_id(const LrGpgSubkey *subkey);
+
+/** Get fingerprint of the subkey in hex digit form.
+ * @param key           Input subkey.
+ * @return              returns fingerprint of the subkey in hex digit form.
+ */
+const char *
+lr_gpg_subkey_get_fingerprint(const LrGpgSubkey *subkey);
+
+/** Get the creation timestamp.
+ * @param key           Input subkey.
+ * @return              returns Get the creation timestamp, -1 if invalid, 0 if not available..
+ */
+long int
+lr_gpg_subkey_get_timestamp(const LrGpgSubkey *subkey);
+
+/** Get information if the subkey can be used for signing.
+ * @param key           Input subkey.
+ * @return              returns TRUE if subkey can be used for signing.
+ */
+gboolean
+lr_gpg_subkey_get_can_sign(const LrGpgSubkey *subkey);
 
 /** @} */
 
