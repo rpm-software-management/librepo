b41189563b65b6adc16c0bfd2c4289db3db19ff3
Petr Písař <ppisar@redhat.com>
2024-04-05 12:06:45 +0200

    Add a private dependency on zck to librepo.pc if zchunk support is enabled
    If librepo is built with zchunk support, zchunk's zck.h header file is
    included from librepo public header files, thus applications building
    against librepo needs zchunk header files available at build time.
    Those who only use pkg-config for tracking dependencies, reported
    build failures:
    
        /usr/include/librepo/downloadtarget.h:27:10: fatal error: zck.h: No such file or directory
    
    This patch adds the dependency on zck to librepo.pc.
    
    It enhances Requires.private instead of Requires to prevent from
    contaminating "pkgconf --libs librepo" with -lzck. Contrary, "pkgconf
    --cflags librepo" correctly appends include path to zck header files.
    The dependency on zck.pc is only checked and enforced with "pkgconf
    --cflags librepo" invocation. This behaviour is a feature. See
    <https://github.com/pkgconf/pkgconf/issues/352> and
    <https://github.com/pkgconf/pkgconf/issues/300>.
    
    Fixes: #305

--- librepo/librepo.pc.cmake
+++ librepo/librepo.pc.cmake
@@ -6,7 +6,7 @@ Name: librepo
 Description: Repodata downloading library.
 Version: @VERSION@
 Requires: glib-2.0
-Requires.private: libcurl openssl libxml-2.0
+Requires.private: libcurl openssl libxml-2.0 @PKGCONF_DEPENDENCY_ZCK@
 Libs: -L${libdir} -lrepo
 Libs.private: -lgpgme -lgpg-error
 Cflags: -I${includedir} -D_FILE_OFFSET_BITS=64
